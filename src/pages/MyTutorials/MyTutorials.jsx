a587e6653508753d9b049497a4dc94d4e7c07087
Durjoy96 <mddurjoy853@gmail.com>
2024-12-23 21:36:55 +0600

    tutorial update complete

--- src/pages/MyTutorials/MyTutorials.jsx
+++ src/pages/MyTutorials/MyTutorials.jsx
@@ -4,16 +4,21 @@ import AxiosSecure from "../../hooks/AxiosSecure";
 import { AuthContext } from "../../provider/AuthProvider";
 import Swal from "sweetalert2";
 import toast from "react-hot-toast";
+import { GrUpdate } from "react-icons/gr";
 
 const MyTutorials = () => {
   const [tutorials, setTutorials] = useState([]);
+  const [updateTutorialId, setUpdateTutorialId] = useState(null);
+  const [updateTutorialData, setUpdateTutorialData] = useState([]);
+  const [refresh, setRefresh] = useState(false); //update the table ui after updating tutorial
   const { user } = useContext(AuthContext);
   const useAxios = AxiosSecure();
+
   useEffect(() => {
     useAxios.get(`/tutorials?email=${user.email}`).then((res) => {
       setTutorials(res.data);
     });
-  }, []);
+  }, [refresh]);
 
   const deleteBtnHandler = (id) => {
     Swal.fire({
@@ -22,7 +27,7 @@ const MyTutorials = () => {
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#FF4438",
-      cancelButtonColor: "#177245",
+      cancelButtonColor: "#313140",
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
@@ -43,6 +48,45 @@ const MyTutorials = () => {
           .catch((error) => toast.error(error.message));
       }
     });
+  };
+
+  const updateBtnHandler = (id) => {
+    document.getElementById("my_modal_3").showModal();
+    setUpdateTutorialId(id);
+    const [tutorial] = tutorials.filter((tutorial) => tutorial._id === id);
+    setUpdateTutorialData(tutorial);
+    console.log(tutorial);
+  };
+
+  const updateFormHandler = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const imageUrl = form.imageUrl.value;
+    const language = form.language.value;
+    const price = form.price.value;
+    const review = form.review.value;
+    const description = form.description.value;
+    const tutorial = {
+      name,
+      email,
+      imageUrl,
+      language: language.toLowerCase(),
+      price: Number(price),
+      review: Number(review),
+      description,
+    };
+
+    useAxios
+      .put(`/tutorial/${updateTutorialId}`, tutorial)
+      .then(() => {
+        toast.success("Tutorial updated!");
+        setRefresh((prev) => !prev); //update the table ui
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
   };
   return (
     <>
@@ -102,7 +146,10 @@ const MyTutorials = () => {
                           >
                             Delete
                           </button>
-                          <button className="btn btn-ghost btn-xs bg-primary border-primary text-primary-content hover:bg-primary hover:opacity-80">
+                          <button
+                            onClick={() => updateBtnHandler(tutorial._id)}
+                            className="btn btn-ghost btn-xs bg-primary border-primary text-primary-content hover:bg-primary hover:opacity-80"
+                          >
                             Update
                           </button>
                         </div>
@@ -118,6 +165,142 @@ const MyTutorials = () => {
             </tbody>
           </table>
         </div>
+        {/* update modal */}
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box w-11/12 max-w-5xl">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <div className="grid grid-cols-1 md:grid-cols-[.5fr_1fr] gap-6 md:gap-8 lg:gap-12">
+              <div>
+                <h2 className="text-xl md:text-2xl font-bold text-base-content">
+                  Update Your Tutorial
+                </h2>
+                <p className="text-xs mt-2 md:text-base font-normal text-base-content-secondary">
+                  Easily update and share your tutorial by providing essential
+                  details such as image, description, and content.
+                </p>
+              </div>
+              <div>
+                <form onSubmit={updateFormHandler}>
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6">
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          What is your name?
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Type here"
+                        value={user?.displayName}
+                        className="input input-bordered w-full"
+                      />
+                    </label>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          What is your email address?
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="Type here"
+                        value={user?.email}
+                        className="input input-bordered w-full"
+                      />
+                    </label>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          What is your image url?
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        defaultValue={updateTutorialData.imageUrl}
+                        name="imageUrl"
+                        placeholder="Type here"
+                        className="input input-bordered w-full"
+                      />
+                    </label>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          What language is your tutorial in?
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        name="language"
+                        defaultValue={updateTutorialData.language}
+                        placeholder="eg. English"
+                        className="input input-bordered w-full"
+                      />
+                    </label>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          Set the price for your tutorial
+                        </span>
+                      </div>
+                      <input
+                        type="number"
+                        name="price"
+                        defaultValue={updateTutorialData.price}
+                        placeholder="eg. 5000"
+                        className="input input-bordered w-full"
+                      />
+                    </label>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          Review
+                        </span>
+                      </div>
+                      <input
+                        type="number"
+                        name="review"
+                        placeholder=""
+                        value={updateTutorialData.review}
+                        className="input input-bordered w-full"
+                      />
+                    </label>
+                    <label className="form-control w-full">
+                      <div className="label">
+                        <span className="label-text text-base text-base-content">
+                          Provide a Description
+                        </span>
+                      </div>
+                      <textarea
+                        name="description"
+                        defaultValue={updateTutorialData.description}
+                        className="textarea textarea-bordered"
+                        placeholder="Type here"
+                      ></textarea>
+                    </label>
+                  </div>
+                  <div className="text-right mt-8">
+                    <button
+                      onClick={() =>
+                        document.getElementById("my_modal_3").close()
+                      }
+                      className="primary-btn"
+                    >
+                      {" "}
+                      <GrUpdate /> Update
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </dialog>
       </div>
     </>
   );
