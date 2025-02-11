835a8d7f20687f9b0278b9eb21307807d9180532
Durjoy96 <mddurjoy853@gmail.com>
2025-02-11 17:47:28 +0600

    Update Hero component: Adjust padding for improved layout consistency

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import Stats from "./Stats";
 const Hero = () => {
   return (
     <>
-      <section className="bg-base-100 sm:py-12 lg:py-24 dark:bg-[#222222]">
+      <section className="bg-base-100 py-12 lg:py-24 dark:bg-[#222222]">
         <div className="max-w-screen-xl px-5 container flex flex-col justify-center  mx-auto lg:flex-row lg:justify-between">
           <div className="flex flex-col justify-center text-center rounded-sm lg:max-w-2xl lg:text-left">
             <h1 className="text-5xl font-bold leading-none text-base-content sm:text-6xl dark:text-white/90">
